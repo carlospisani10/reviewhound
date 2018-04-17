1ef44268b902d5008d4d597d003a692055ec16a8
Mike Barajas <mikebarajas@live.com>
2018-04-17 16:38:13 -0500

    pushing working flask, need to clean it up

---
+++ website build/app3.py
@@ -0,0 +1,113 @@
+import os
+import io
+import numpy as np
+from PIL import Image
+import base64
+from flask import Flask, render_template, request
+import os
+import pandas as pd
+import json 
+import numpy as np
+import keras.models
+import re
+import sys
+sys.path.append(os.path.abspath("./Sentiment_Model"))
+#from load import *
+import logging
+from keras.preprocessing import sequence
+from keras.preprocessing.text import text_to_word_sequence
+from keras.models import Sequential
+from keras.layers import MaxPooling1D, Conv1D, Flatten, Dropout, Dense
+from keras.layers.embeddings import Embedding
+from keras.preprocessing import sequence
+import tensorflow as tf
+
+import keras
+from keras.preprocessing import image
+from keras.preprocessing.image import img_to_array
+from keras import backend as K
+
+from flask import Flask, request, redirect, jsonify, render_template
+
+app = Flask(__name__)
+model = None
+graph = None
+
+PATH = './Sentiment_Model/optimal_dict3.json'
+with open(PATH) as json_data:
+    d = json.load(json_data)
+word_dict = pd.Series(d)
+
+# Parameters
+version = 4
+words = len(word_dict)
+review_len = 1000
+vec_len = 300
+patience = 5
+batch_size = 40
+epochs = 3
+
+def load_model():
+    global model
+    global graph
+    model = keras.models.load_model("./Sentiment_Model/optimalfloyds3.h5")
+    graph = K.get_session().graph
+
+load_model()
+
+#econding functions
+def encode_sentence(text):
+    result = []
+    arr = text_to_word_sequence(text, lower=True, split=" ")
+    for word in arr:
+        w = encode_word(word)
+        if w is not None:
+            result.append(w)
+    return result
+
+def encode_word(word):
+    if word not in word_dict:
+        return None
+    return word_dict[word]
+
+def encode_batch(arr):
+    result = []
+    for sentence in arr:
+        result.append(encode_sentence(sentence))
+    return sequence.pad_sequences(result, maxlen=review_len)
+
+def predict_batch(arr):
+    batch = encode_batch(arr)
+    result = model.predict(batch, batch_size=len(batch), verbose=0)
+    return result
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    data = {"success": False}
+    if request.method == 'POST':
+        if request.form.get('userInput'):
+            # read the base64 encoded string
+            review = request.form.get('userInput')
+
+            # Get the tensorflow default graph
+            global graph
+            with graph.as_default():
+
+                # Use the model to make a prediction
+
+                # prediction = predict_batch(review)
+                predictions = predict_batch([review])
+                # data["prediction"] = str(prediction)
+                data["review"] = review
+                data["predictions"] = str(predictions)
+
+                # indicate that the request was a success
+                data["success"] = True
+
+        return jsonify(data)
+    return render_template("sniffer.html")
+
+if __name__ == "__main__":
+    load_model()
+    app.run()
--- website build/static/js/app.js
+++ website build/static/js/app.js
@@ -1,55 +1,72 @@
-var button = document.getElementById("button");
+// var button = document.getElementById("button");
 
-button.onclick = function () {
-    // listen for user input
-    console.log("Clicked!!!")
-    // FUNCTION PLACEHOLDER
-    var userInput = document.getElementById("userInput").value;
+// button.onclick = function () {
+//     // listen for user input
+//     console.log("Clicked!!!")
+//     // FUNCTION PLACEHOLDER
+//     var userInput = document.getElementById("userInput").value;
     
-    // Send input to APIs
-    fraudAnalysis(userInput)
-    sentimentAnalysis(userInput)
+//     // Send input to APIs
+//     // fraudAnalysis(userInput)
+//     sentimentAnalysis(userInput)
+// };
+
+// function fraudAnalysis(userInput) {
+//     // Send user input to fraudAnalysis API
+//     console.log(userInput)
+//     data = String(userInput)
+//     console.log(data)
+//     // FUNCTION PLACEHOLDER
+
+//     // Visualize Output
+//     displayFraud(data)
+// }
+
+function handleClear() {
+    var canvas = document.getElementById("userInput");
+    var context = canvas.getContext("2d");
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.rect(0, 0, canvasWidth, canvasHeight);
+    context.fillStyle = 'white';
+    context.fill();
+}
+function handleSubmit() {
+    var canvas = document.getElementById("userInput");
+    var dataURL = canvas.toDataURL("userInput");
+    document.getElementById('userInput').value = dataURL;
+    document.forms["form1"].submit();
 };
 
+// function sentimentAnalysis(userInput) {
+//     // Send user input to sentimentAnalysis API
+//     console.log(userInput)
+//     user_input = String(userInput)
+//     console.log(user_input)
+//     // FUNCTION PLACEHOLDER
+//     function handleSubmit() {
+//         var canvas = document.getElementById("canvas");
+//         var dataURL = canvas.toDataURL("image/png");
+//         document.getElementById('digit').value = dataURL;
+//         document.forms["form1"].submit();
+//     };
+//     // Visualize Output
+//     displaySentiment(data)
+// }
 
+// // function displayFraud(data) {
+// //     // Reset DIV
+// //     d3.select("#fraudDisplay").remove();
+// //     d3.select("#fraudContainer").html('<div id="fraudDisplay" class="span" style="width: 100%; height: 800px; border: 3px solid #AAA;"></div>');
+// //     console.log(userInput.value)
+// //     // Visualize analysis
 
+// // }
 
-function fraudAnalysis(userInput) {
-    // Send user input to fraudAnalysis API
-    console.log(userInput)
-    data = String(userInput)
-    console.log(data)
-    // FUNCTION PLACEHOLDER
+// function displaySentiment(data) {
+//     // Reset DIV
+//     d3.select("#sentimentDisplay").remove();
+//     d3.select("#fraudContainer").html('<div id="sentimentDisplay" class="span" style="width: 100%; height: 800px; border: 3px solid #AAA;"></div>');
+//     console.log(userInput.value)
+//     // Visualize analysis
 
-    // Visualize Output
-    displayFraud(data)
-}
-
-function sentimentAnalysis(userInput) {
-    // Send user input to sentimentAnalysis API
-    console.log(userInput)
-    user_input = String(userInput)
-    console.log(user_input)
-    // FUNCTION PLACEHOLDER
-
-    // Visualize Output
-    displaySentiment(data)
-}
-
-function displayFraud(data) {
-    // Reset DIV
-    d3.select("#fraudDisplay").remove();
-    d3.select("#fraudContainer").html('<div id="fraudDisplay" class="span" style="width: 100%; height: 800px; border: 3px solid #AAA;"></div>');
-    console.log(userInput.value)
-    // Visualize analysis
-
-}
-
-function displaySentiment(data) {
-    // Reset DIV
-    d3.select("#sentimentDisplay").remove();
-    d3.select("#fraudContainer").html('<div id="sentimentDisplay" class="span" style="width: 100%; height: 800px; border: 3px solid #AAA;"></div>');
-    console.log(userInput.value)
-    // Visualize analysis
-
-}
+// }
